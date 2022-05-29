9956d4a784efb4a7b9644b84c3b3856c462c48e4
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2022-05-30 01:35:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const graph = new simpleGraph.Map();
+const map = new simpleGraph.Map();
 
 ////////////// -- many to one mapping -- //////////////
 // myMap.set([<key1> ,<key2>,...] , <values>);
-graph.set(1 ,[4, 5])
+map.set(1 ,[4, 5])
      .set(2, [5])
      .set(4, [1, 5, 6])
      .set(5, [1, 2, 4])
      .set(6, [1, 4, 7])
      .set(7, [6])
 
-const paths = graph.createGraph().findShortestPath(1, 7);
+const paths = map.createGraph().findShortestPath(1, 7);
 
 console.log(paths);
